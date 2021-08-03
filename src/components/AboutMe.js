39d0ed2fa6b8ef146bd5563bcde96042ffa80071
AzaharaGV <azahara1359@gmail.com>
2021-08-03 12:56:33 +0200

    specific_pics

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Foto from "../images/itsme.PNG";
+import foto_foto from "../images/fotografia.jpg";
+import foto_design from "../images/diseño.jpg";
+import foto_progra from "../images/programacion.jpg";
 
 function AboutMe() {
   return (
@@ -16,19 +19,19 @@ function AboutMe() {
           <div>
             <span className="aboutme__title">Programación</span>
             <a>
-              <img className="programacion_img" src={Foto}></img>
+              <img className="programacion_img" src={foto_progra}></img>
             </a>
           </div>
           <div>
             <span className="aboutme__title">Fotografía</span>
             <a>
-              <img className="fotografia_img" src={Foto}></img>
+              <img className="fotografia_img" src={foto_foto}></img>
             </a>
           </div>
           <div>
             <span className="aboutme__title">Diseño</span>
             <a>
-              <img className="diseño_img" src={Foto}></img>{" "}
+              <img className="diseño_img" src={foto_design}></img>{" "}
             </a>
           </div>
         </div>
