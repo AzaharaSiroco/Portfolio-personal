30a85da618231d8885695fde39ef672ff37dd533
AzaharaSiroco <Azahara1359@gmail.com>
2021-09-26 12:26:00 +0200

    Photography

--- src/components/Photography.js
+++ src/components/Photography.js
@@ -1,13 +1,35 @@
 import React from "react";
 
+import foto1 from "../images/fotografias/PRISMA-3-6.jpg";
+import foto2 from "../images/fotografias/SEBASTIANMALASPINA_AZAHARASIROCO-1-15.jpg";
+import foto3 from "../images/fotografias/SEBASTIANMALASPINA_AZAHARASIROCO-1-20.jpg";
+import foto4 from "../images/fotografias/SEBASTIANMALASPINA_AZAHARASIROCO-1-25.jpg";
+import foto5 from "../images/fotografias/SEBASTIANMALASPINA_AZAHARASIROCO-1-30copia.jpg";
+import foto6 from "../images/fotografias/SEBASTIANMALASPINA_AZAHARASIROCO-1-38.jpg";
+
 function Photography() {
   return (
     <>
-      <div className="photography_container">
-        <section className="photography_container">
-          <h1 className="photography_title">FOTOGRAFIA</h1>
-        </section>
-      </div>
+      <section className="photography_container">
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto1}></img>
+        </div>
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto2}></img>
+        </div>
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto3}></img>
+        </div>
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto4}></img>
+        </div>
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto5}></img>
+        </div>
+        <div className="fotografias">
+          <img alt="foto" className="foto_img" src={foto6}></img>
+        </div>
+      </section>
     </>
   );
 }
